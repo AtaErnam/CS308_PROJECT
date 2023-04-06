745422904fd594e5d1e726a54a2ee4e2097a3c93
Ata Ernam <ataernam@sabanciuniv.edu>
2023-04-06 12:03:42 +0300

    implemented the app and server

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const userRouter = require("./routes/userRoutes");
+const categoryRouter = require("./routes/categoryRoutes");
+const productRouter = require("./routes/productRoutes");
+
+const app = express();
+
+// 1) Middlewares
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Hello from the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
+
+// 2) Routes
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/product", productRouter);
+app.use("/api/v1/category", categoryRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Cant find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connected"));
+
+const port = process.env.port || 3008;
+const server = app.listen(port, () => {
+  console.log("HELLOO");
+});
