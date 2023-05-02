6f87723c0d1c0c41e57825ec44d6b88fb2346332
helinvaroglu <helinv@sabanciuniv.edu>
2023-05-03 02:46:05 +0300

    Implemented routing mechanism for single page application

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CartPage from "./components/CartPage/CartPage"
+import ProductsPage from "./components/ProductsPage/ProductsPage"
 
 function App() {
   const [message, setMessage] = useState("");
@@ -11,9 +16,20 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>{message}</h1>
-    </div>
+    <ChakraProvider>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact element={<ProductsPage/>} />
+          {/* <Route path="/signin" component={Signin} /> */}
+          {/* <Route path="/signup" component={Signup} /> */}
+          <Route path="/cart" element={<CartPage/>} />
+        </Routes>
+      </Router>
+      <div className="App">
+        <h1>{message}</h1>
+      </div>
+    </ChakraProvider>
   );
 }
 
