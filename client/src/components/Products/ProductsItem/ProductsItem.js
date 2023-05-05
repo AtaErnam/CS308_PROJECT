8090da85dcb1b65a67b82aa66a41383b50a9beef
hayrettins@sabanciuniv.edu <104569000+hayrettins@users.noreply.github.com>
2023-05-05 19:30:46 +0300

    rating and commenting

--- client/src/components/Products/ProductsItem/ProductsItem.js
+++ client/src/components/Products/ProductsItem/ProductsItem.js
@@ -19,6 +19,9 @@ const ProductsItem = (props) => {
   const cartCtx = useContext(CartContext);
   const price = `₺${props.price.toFixed(2)}`;
   const [isOutOfStockOpen, setIsOutOfStockOpen] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
+  const [submittedReviews, setSubmittedReviews] = useState([]);
 
   const addToCartHandler = (amount) => {
     if (props.stock === 0) {
@@ -34,6 +37,22 @@ const ProductsItem = (props) => {
       category: props.category,
       stock: props.stock,
     });
+  };
+
+  const handleRatingChange = (event) => {
+    setRating(event.target.value);
+  };
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleReviewSubmit = (event) => {
+    event.preventDefault();
+    const newReview = { rating: rating, comment: comment };
+    setSubmittedReviews([...submittedReviews, newReview]);
+    setRating(0);
+    setComment('');
   };
 
   return (
@@ -54,6 +73,39 @@ const ProductsItem = (props) => {
         <h2>Out of Stock!</h2>
         <button onClick={() => setIsOutOfStockOpen(false)}>Close</button>
       </Modal>
+      <div>
+        <h4>Reviews</h4>
+        {submittedReviews.length === 0 ? (
+          <p>No reviews yet.</p>
+        ) : (
+          <ul>
+            {submittedReviews.map((review, index) => (
+              <li key={index}>
+                <div>Rating: {review.rating}</div>
+                <div>Comment: {review.comment}</div>
+              </li>
+            ))}
+          </ul>
+        )}
+        <form onSubmit={handleReviewSubmit}>
+          <div>
+            <label htmlFor="rating">Rating:</label>
+            <select id="rating" name="rating" value={rating} onChange={handleRatingChange}>
+              <option value="0">Select rating</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="comment">Comment:</label>
+          <textarea id="comment" name="comment" value={comment} onChange={handleCommentChange}></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>  
     </li>
   );
 };
